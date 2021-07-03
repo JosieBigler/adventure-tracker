1499250ef03a7fff7fa7bd3dde590c77f7e42868
Josie Bigler <jbigler01@hamline.edu>
2021-07-02 22:19:44 -0500

    Dialog box working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogOverviewExampleDialog } from './app.component';
 import { AdventureComponent } from './adventure/adventure.component';
-import {MatDialogModule} from '@angular/material/dialog'
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule, } from '@angular/material/form-field';
+import { MatInputModule, } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdventureComponent,
+    DialogOverviewExampleDialog,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatDialogModule,
+    MatFormFieldModule,
     BrowserAnimationsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
